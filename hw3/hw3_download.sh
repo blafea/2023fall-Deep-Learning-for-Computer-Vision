b7bf8fe2529289957c8f4d299f0cacf0544e9a6e
blafea <blafea221651@gmail.com>
2023-11-28 16:51:48 +0800

    download edited

--- hw3/hw3_download.sh
+++ hw3/hw3_download.sh
@@ -1,1 +1,5 @@
 gdown "https://drive.google.com/u/3/uc?id=1oW4FLcqsVt9RltRgWZTKY0txdxtBGTd5&export=download" -O "best_model.pth"
+
+python3 -c "import clip; clip.load('ViT-L/14')"
+python3 -c "import clip; clip.load('ViT-B/32')"
+python3 -c "import timm; timm.create_model('vit_large_patch14_clip_224.openai', pretrained=True)"
